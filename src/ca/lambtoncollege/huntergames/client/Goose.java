a43fbaeead950b3e2719a24460ee3bb7fcd26c3b
c0650224 <c0650224@SB-N208-37.lambton.on.ca>
2014-11-02 20:27:21 -0500

    basic class

---
+++ src/ca/lambtoncollege/huntergames/client/Goose.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.lambtoncollege.huntergames.client;
+
+/**
+ *
+ * @author alpenliebe
+ */
+public class Goose {
+    
+}
