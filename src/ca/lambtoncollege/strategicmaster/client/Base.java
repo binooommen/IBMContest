c9f80118b35fe5cc687823750a55ce6f1edbbfef
c0650224 <c0650224@SB-N208-03.lambton.on.ca>
2014-11-06 22:56:32 -0500

    basic building 1.0

--- src/ca/lambtoncollege/strategicmaster/client/Base.java
+++ src/ca/lambtoncollege/strategicmaster/client/Base.java
@@ -12,7 +12,7 @@ import java.util.LinkedList;
  *
  * @author alpenliebe
  */
-public class Base {
+public class Base extends Node {
     private int team;
     private LinkedList baseList = null;
     private Unit unit;
@@ -24,8 +24,41 @@ public class Base {
     }
     
     Base(int amount){
-        this();
-        this.unit.setAmount(amount);
+        this.team=0;
+        this.baseList = new LinkedList();
+        this.unit=new Unit(this.team,amount);
     }
     
+    Base(int team,int amount){
+        this.team=team;
+        this.baseList = new LinkedList();
+        this.unit=new Unit(team,amount);
+    }
+
+    public void setTeam(int team) {
+        this.team = team;
+        this.unit.setTeam(team);
+    }
+    
+    public boolean isEmpty(){
+        return this.unit.isEmpty();
+    }
+    
+    public void enter(Unit newUnit){
+        if(this.unit.getTeam()==newUnit.getTeam()){
+            this.unit.add(newUnit.getAmount());
+        } else {
+            int amount = this.unit.comparedTo(newUnit);
+            if(amount < 0) setTeam(newUnit.getTeam());
+            this.unit.setAmount((amount < 0) ? -amount : amount);
+        }
+    }
+    
+    public Unit select(boolean selectAll){
+        return selectAll ? this.unit.split(this.unit.getAmount()) : this.unit.split(this.unit.getHalf());
+    }
+    
+    public void release(Unit newUnit){
+       this.unit.add(newUnit.getAmount());
+    }
 }
--- src/ca/lambtoncollege/strategicmaster/client/Unit.java
+++ src/ca/lambtoncollege/strategicmaster/client/Unit.java
@@ -11,13 +11,16 @@ package ca.lambtoncollege.strategicmaster.client;
  * @author alpenliebe <alpseinstein@gmail.com>
  */
 public class Unit {
+    private int team;
     private int amount;
     
     Unit(){
+       this.team=0;
        this.amount=0; 
     }
     
-    Unit(int amount){
+    Unit(int team,int amount){
+        this.team=team;
         this.amount=amount;
     }
 
@@ -28,16 +31,41 @@ public class Unit {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    public int getTeam() {
+        return team;
+    }
+    
+    public void setTeam(int team) {
+        this.team = team;
+    }
     
     public void add(int amount){
-        this.amount-=amount;
+        this.amount+=amount;
     }
     
     public void remove(int amount){
         this.amount-=amount;
     }
     
+    public int getHalf(){
+        return amount>1 ? amount/2 : amount==1 ? 1 : 0;
+    }
+    
+    public Unit split(int n){
+        amount-=n;
+        return new Unit(getTeam(),n);
+    }
+    
+    public boolean isEmpty(){
+        return amount==0 ;
+    }
+    
     public int comparedTo(Unit unit){
         return this.amount-unit.getAmount();
+    }
+    
+    public void capture(Base base){
+        
     }
 }
